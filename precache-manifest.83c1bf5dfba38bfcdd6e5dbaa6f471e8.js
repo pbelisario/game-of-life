4169af38fcd65d4a764f668d1481ff21425c7611
pbelisario_win <pedrobelisario1997@gmail.com>
2020-05-22 22:03:33 -0300

    Updates

---
+++ precache-manifest.83c1bf5dfba38bfcdd6e5dbaa6f471e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab0b2607dc14029aa32da80906ae9feb",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "613a37f8027a2a5abc7f",
+    "url": "/game-of-life/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d011d77ef3238d2023b9",
+    "url": "/game-of-life/static/js/2.1be4d48f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game-of-life/static/js/2.1be4d48f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "613a37f8027a2a5abc7f",
+    "url": "/game-of-life/static/js/main.cffdd9c3.chunk.js"
+  },
+  {
+    "revision": "74c0ec62b28c760df103",
+    "url": "/game-of-life/static/js/runtime-main.ccc8ab68.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/game-of-life/static/media/logo.5d5d9eef.svg"
+  }
+]);
